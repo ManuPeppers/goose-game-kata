98e36c86d992feea2de8d1b7926781ab70a04aa9
ManuPeppers <manuel.grimaldi@xpeppers.com>
2019-07-12 11:30:34 +0200

    manages duplicated players

--- src/main/java/PlayerManager.java
+++ src/main/java/PlayerManager.java
@@ -5,9 +5,24 @@ public class PlayerManager {
 
     List<Player> playerList = new ArrayList<>();
 
-    public void addPlayer(String input) {
+    public String addPlayer(String input) {
         String name = input.split(" ")[2];
+
+        if (playerExists(name))
+            return name + ": already existing player";
+
         playerList.add(new Player(name));
+
+        return extractParticipants();
+    }
+
+    private boolean playerExists(String name) {
+        for (Player player : playerList) {
+            if (name.equals(player.name())) {
+                return true;
+            }
+        }
+        return false;
     }
 
     public String extractParticipants() {
--- src/test/java/PlayerManagerTest.java
+++ src/test/java/PlayerManagerTest.java
@@ -1,24 +1,36 @@
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
 public class PlayerManagerTest {
 
+    PlayerManager playerManager;
+
+    @Before
+    public void setUp() {
+        playerManager = new PlayerManager();
+    }
+
     @Test
-    public void insertOnePlayer(){
-        PlayerManager playerManager =new PlayerManager();
+    public void insertOnePlayer() {
         playerManager.addPlayer("add player Pippo");
 
         assertEquals("players: Pippo", playerManager.extractParticipants());
     }
 
     @Test
-    public void insertTwoPlayers(){
-        PlayerManager playerManager =new PlayerManager();
+    public void insertTwoPlayers() {
         playerManager.addPlayer("add player Pippo");
         playerManager.addPlayer("add player Pluto");
 
         assertEquals("players: Pippo, Pluto", playerManager.extractParticipants());
     }
 
+    @Test
+    public void shouldReturnErrorIfPlayerAlreadyExists() {
+        playerManager.addPlayer("add player Pippo");
+
+        assertEquals("Pippo: already existing player", playerManager.addPlayer("add player Pippo"));
+    }
 }
